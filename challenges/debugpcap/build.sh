f7fdac33a5fe440d99a799ad3377d8fabba81146
Jonas Rudloff <jonas@x250>
2017-07-31 00:44:17 +0200

    more better

--- challenges/debugpcap/build.sh
+++ challenges/debugpcap/build.sh
@@ -2,7 +2,9 @@
 
 docker build -t debugpcap .
 
-tcpdump -i docker0 'tcp port 1234' -G 5 -w debug.pcap &
+tcpdump -i docker0 'tcp port 1234' -w debug.pcap &
+TCPDUMP=$!
 docker run --rm -d --name debuggee --cap-add SYS_PTRACE --security-opt seccomp:unconfined debugpcap ./debuggee.sh
 docker run --rm --name debugger --link debuggee debugpcap ./debugger.sh
-sleep 7
+kill $TCPDUMP
+sleep 1
