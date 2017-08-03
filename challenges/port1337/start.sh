7e56ec69c36ec720435021eaa7d62d9e0992e9d8
Kristoffer Søholm <k.soeholm@gmail.com>
2017-08-03 18:53:19 +0200

    Add udp to port1337 challenge

--- challenges/port1337/start.sh
+++ challenges/port1337/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 socat sctp-listen:1337,fork exec:"cat flag" &
 echo $! > .flag_server_pid
+ncat -c "cat udp" -k -u -l 1337 &
+echo $! > .udp_server_pid
 socat tcp-listen:1337,fork exec:"cat tcp" &
-echo $! > .troll_server_pid
-
+echo $! > .tcp_server_pid
--- challenges/port1337/stop.sh
+++ challenges/port1337/stop.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 kill $(cat .flag_server_pid)
-kill $(cat .troll_server_pid)
+kill $(cat .tcp_server_pid)
+kill $(cat .udp_server_pid)
 rm .flag_server_pid
-rm .troll_server_pid
+rm .tcp_server_pid
+rm .udp_server_pid
