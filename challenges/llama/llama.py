918bec3197175f0a03b2075d28018f85ab28a467
Morten Brøns-Pedersen <f@ntast.dk>
2017-08-19 00:10:58 +0200

    fixed flag format

--- challenges/llama/llama.py
+++ challenges/llama/llama.py
@@ -9,5 +9,5 @@ A = lambda f: lambda n: 5 if not (n >> 1) else Y(M)((f(n-1), Y(C)(n)))
 l = 0x6c6c616d612c6c6c616d612c6c6c616d61
 g = lambda x: Y(A)(x) % l
 
-flag = '{%x}' % g(0x1337)
+flag = 'FLAG{%x}' % g(0x1337)
 print 'flag:', flag
